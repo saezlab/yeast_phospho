76cd57cbe72400a8574351a8e3dcc18ed083b466
Emanuel Goncalves <emanuelvgoncalves@gmail.com>
2015-11-08 08:34:46 +0000

    Linear regression added to define associations between metabolites and TF/kinases

--- src/yeast_phospho/analysis/kinase_ko_validation.py
+++ src/yeast_phospho/analysis/kinase_ko_validation.py
@@ -26,67 +26,29 @@ metabolomics.index = [str(i) for i in metabolomics.index]
 k_activity = read_csv('%s/tables/kinase_activity_dynamic.tab' % wd, sep='\t', index_col=0)
 k_activity = k_activity[(k_activity.count(1) / k_activity.shape[1]) > .75].replace(np.NaN, 0.0)
 
+#
+metabolomics_ss = read_csv('%s/tables/metabolomics_steady_state.tab' % wd, sep='\t', index_col=0)
+metabolomics_ss.index = [str(i) for i in metabolomics_ss.index]
 
 # Linear regression
 y = metabolomics.T
 x = k_activity[y.index].T
 
-L1_wt_list = [0, .5, 1.]
-alpha_list = [.1, 1., 10.]
-nfeat_list = [1, 5, 10]
-
-x_coefs = {}
-for m in y:
-    # Train parameters
-    res = (np.Inf, np.Inf, np.Inf, np.Inf)
-    for L1_wt, alpha, n_feat in it.product(L1_wt_list, alpha_list, nfeat_list):
-        fs = SelectKBest(f_regression, k=n_feat).fit(x, y[m]).get_support()
-
-        xs = x.loc[:, fs]
-        xs = st.add_constant(xs)
-
-        cv = ShuffleSplit(len(y[m]), n_iter=30, test_size=.15)
-
-        p_rmse = np.median([rmse(y.ix[test, m], sm.OLS(y.ix[train, m], xs.ix[train]).fit_regularized(alpha=alpha, L1_wt=L1_wt).predict(xs.ix[test])) for train, test in cv])
-
-        if res[0] > p_rmse:
-            res = (p_rmse, L1_wt, alpha, n_feat)
-
-    p_rmse, L1_wt, alpha, n_feat = res
-
-    # Fit model
-    fs = SelectKBest(f_regression, k=n_feat).fit(x, y[m]).get_support()
-
-    xs = x.loc[:, fs]
-    xs = st.add_constant(xs)
-
-    lm = sm.OLS(y[m], xs).fit_regularized(alpha=alpha, L1_wt=L1_wt)
-
-    x_coefs[m] = lm
-
-    print m, p_rmse, L1_wt, alpha, n_feat, lm.rsquared, lm.f_pvalue
-
-x_coefs = {m: x_coefs[m] for m in x_coefs if x_coefs[m].f_pvalue < 0.05}
+x_coefs = {m: {f: sm.OLS(y[m], st.add_constant(x[f])).fit() for f in x} for m in y}
 print '[INFO] Regressions done: ', len(x_coefs)
 
-
-# Interactions table
-df = [(f, m, x_coefs[m].rsquared_adj, x_coefs[m].params[f], x_coefs[m].pvalues[f], x_coefs[m].conf_int().ix[f, 0], x_coefs[m].conf_int().ix[f, 1]) for m in x_coefs for f, p in x_coefs[m].pvalues.to_dict().items() if f != 'const' and m in met_name]
-df = DataFrame(df, columns=['kinase', 'metabolite', 'rsquared_adj', 'coef', 'pvalue', 'ci_lb', 'ci_ub']).dropna().sort('pvalue')
-df['metabolite_name'] = [met_name[i] for i in df['metabolite']]
-df['kinase_name'] = [acc_name[i] for i in df['kinase']]
-print df[df['pvalue'] < .1].sort('rsquared_adj', ascending=False)
-df.sort('pvalue').sort('rsquared_adj', ascending=False).to_csv('%s/tables/kinases_coefficients.txt' % wd, sep='\t', index=False)
-
-
-#
-metabolomics_ss = read_csv('%s/tables/metabolomics_steady_state.tab' % wd, sep='\t', index_col=0)
-metabolomics_ss.index = [str(i) for i in metabolomics_ss.index]
+df = DataFrame([(m, f, x_coefs[m][f].params[f], x_coefs[m][f].pvalues[f]) for f in x for m in y], columns=['variable', 'feature', 'coef', 'pvalue']).sort('pvalue')
+df['cor'] = [pearson(y[m], x[f])[0] for m, f in df[['variable', 'feature']].values]
+df['ci_lb'] = [x_coefs[m][f].conf_int().ix[f, 0] for m, f in df[['variable', 'feature']].values]
+df['ci_ub'] = [x_coefs[m][f].conf_int().ix[f, 1] for m, f in df[['variable', 'feature']].values]
+df['var_name'] = [met_name[i] if i in met_name else i for i in df['variable']]
+df['feature_name'] = [acc_name[i] for i in df['feature']]
+df.to_csv('%s/tables/tf_coefficients.txt' % wd, sep='\t', index=False)
+print df.head(15)
 
 
-#
-plot_df = df[[k in metabolomics_ss and m in metabolomics_ss.index for k, m in df[['kinase', 'metabolite']].values]]
-plot_df['fold-change'] = [metabolomics_ss.ix[m, k] for k, m in plot_df[['kinase', 'metabolite']].values]
+plot_df = [(m, f, coef, metabolomics_ss.ix[m, c]) for m, f, coef, pvalue in df[['variable', 'feature', 'coef', 'pvalue']].values if pvalue < 0.01 and m in metabolomics_ss.index for c in metabolomics_ss if f in c]
+plot_df = DataFrame(plot_df, columns=['variable', 'feature', 'coef', 'fold-change'])
 
 sns.set(style='ticks')
 margin = 1.20
--- src/yeast_phospho/analysis/tf_ko_validation.py
+++ src/yeast_phospho/analysis/tf_ko_validation.py
@@ -8,6 +8,7 @@ from yeast_phospho import wd, data
 from sklearn.cross_validation import KFold, ShuffleSplit
 from statsmodels.tools.eval_measures import rmse
 from scipy.stats.stats import spearmanr, pearsonr
+from sklearn.linear_model import Lasso, RandomizedLasso
 from sklearn.feature_selection.univariate_selection import SelectKBest, f_regression
 from pandas import DataFrame, Series, read_csv, concat, melt, pivot_table
 from yeast_phospho.utilities import pearson, get_proteins_name, get_metabolites_name
@@ -42,10 +43,6 @@ def import_tf_ko(file_path):
     df = df[list(set(df).intersection(tf_ko_map))]
     df.columns = [c.split('_')[0] + '_' + tf_ko_map[c] for c in df.columns]
 
-    df = df.ix[metabolomics.index].dropna()
-
-    df = df.loc[:, [c.split('_')[1] in tf_activity.index for c in df]]
-
     return df
 
 tf_logfc = import_tf_ko('%s/yeast_tf/yeast_tf_ko_logfc.txt' % data)
@@ -56,56 +53,27 @@ tf_pvalue = import_tf_ko('%s/yeast_tf/yeast_tf_ko_pvalues.txt' % data)
 y = metabolomics.T
 x = tf_activity[y.index].T
 
-L1_wt_list = [0, .5, 1.]
-alpha_list = [.1, 1., 10.]
-nfeat_list = [1, 5, 10]
-
-x_coefs = {}
-for m in y:
-    # Train parameters
-    res = (np.Inf, np.Inf, np.Inf, np.Inf)
-    for L1_wt, alpha, n_feat in it.product(L1_wt_list, alpha_list, nfeat_list):
-        fs = SelectKBest(f_regression, k=n_feat).fit(x, y[m]).get_support()
-
-        xs = x.loc[:, fs]
-        xs = st.add_constant(xs)
-
-        cv = ShuffleSplit(len(y[m]), n_iter=30, test_size=.15)
-
-        p_rmse = np.median([rmse(y.ix[test, m], sm.OLS(y.ix[train, m], xs.ix[train]).fit_regularized(alpha=alpha, L1_wt=L1_wt).predict(xs.ix[test])) for train, test in cv])
-
-        if res[0] > p_rmse:
-            res = (p_rmse, L1_wt, alpha, n_feat)
-
-    p_rmse, L1_wt, alpha, n_feat = res
-
-    # Fit model
-    fs = SelectKBest(f_regression, k=n_feat).fit(x, y[m]).get_support()
-
-    xs = x.loc[:, fs]
-    xs = st.add_constant(xs)
-
-    lm = sm.OLS(y[m], xs).fit_regularized(alpha=alpha, L1_wt=L1_wt)
-
-    x_coefs[m] = lm
-
-    print m, p_rmse, L1_wt, alpha, n_feat, lm.rsquared, lm.f_pvalue
-
-x_coefs = {m: x_coefs[m] for m in x_coefs if x_coefs[m].f_pvalue < 0.05}
+x_coefs = {m: {f: sm.OLS(y[m], st.add_constant(x[f])).fit() for f in x} for m in y}
 print '[INFO] Regressions done: ', len(x_coefs)
 
-df = [(f, m, x_coefs[m].params[f], x_coefs[m].pvalues[f], x_coefs[m].conf_int().ix[f, 0], x_coefs[m].conf_int().ix[f, 1]) for m in x_coefs if m in tf_logfc.index for f, p in x_coefs[m].pvalues.to_dict().items() if f != 'const']
-df = [(tf, m, c, v, p, tf_logfc.ix[m, c], tf_pvalue.ix[m, c], cilb, ciub) for tf, m, v, p, cilb, ciub in df for c in [c for c in tf_logfc if tf in c]]
-df = DataFrame(df, columns=['TF', 'Metabolite', 'Condition', 'coef', 'coef_pvalue', 'fold-change', 'pvalue', 'ci_lb', 'ci_ub']).dropna()
-df['Metabolite_name'] = [met_name[i] for i in df['Metabolite']]
-df['TF_name'] = [acc_name[i] for i in df['TF']]
-print df.sort('pvalue')
-df.sort(['coef_pvalue', 'pvalue']).to_csv('%s/tables/tf_coefficients.txt' % wd, sep='\t', index=False)
+df = DataFrame([(m, f, x_coefs[m][f].params[f], x_coefs[m][f].pvalues[f]) for f in x for m in y], columns=['variable', 'feature', 'coef', 'pvalue']).sort('pvalue')
+df['cor'] = [pearson(y[m], x[f])[0] for m, f in df[['variable', 'feature']].values]
+df['ci_lb'] = [x_coefs[m][f].conf_int().ix[f, 0] for m, f in df[['variable', 'feature']].values]
+df['ci_ub'] = [x_coefs[m][f].conf_int().ix[f, 1] for m, f in df[['variable', 'feature']].values]
+df['var_name'] = [met_name[i] for i in df['variable']]
+df['feature_name'] = [acc_name[i] for i in df['feature']]
+df.to_csv('%s/tables/tf_coefficients.txt' % wd, sep='\t', index=False)
+print df.head(15)
+
+
+plot_df = [(m, f, coef, tf_logfc.ix[m, c], pvalue) for m, f, coef, pvalue in df[['variable', 'feature', 'coef', 'pvalue']].values if pvalue < 0.01 and m in tf_logfc.index for c in tf_logfc if f in c]
+plot_df = DataFrame(plot_df, columns=['variable', 'feature', 'coef', 'fold-change', 'pvalue'])
+plot_df['log10_pvalue'] = [-np.log10(p) if c > 0 else np.log10(p) for p, c in plot_df[['pvalue', 'coef']].values]
 
 sns.set(style='ticks')
 margin = 1.20
-xlim, ylim = (df['coef'].min() * margin, df['coef'].max() * margin), (df['fold-change'].min() * margin, df['fold-change'].max() * margin)
-sns.jointplot('coef', 'fold-change', data=df, kind='reg', xlim=xlim, ylim=ylim, marginal_kws={'hist': False}, stat_func=pearsonr)
+xlim, ylim = (plot_df['coef'].min() * margin, plot_df['coef'].max() * margin), (plot_df['fold-change'].min() * margin, plot_df['fold-change'].max() * margin)
+sns.jointplot('log10_pvalue', 'fold-change', data=plot_df, kind='reg', xlim=xlim, ylim=ylim, marginal_kws={'hist': False}, stat_func=pearsonr)
 plt.axhline(y=0, ls='--', c='.5', lw=.3)
 plt.axvline(x=0, ls='--', c='.5', lw=.3)
 plt.xlabel('TF (coefficient)')
